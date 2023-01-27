9aa2ab962ba261cbf49c8bad85be761257f2cf9d
Rodrigo <rodrigodadam@gmail.com>
2023-01-27 11:30:51 +0000

    class homeControllers inserted

--- index.php
+++ index.php
@@ -10,14 +10,8 @@
 
 	spl_autoload_register($autoload);
 
-	$pdo = \MySql::conectar();
-
-	Router::get('/home/',function(){
-		echo '<h2> Home </h2>';
-	});
-
-	Router::get('/home/?', function($par){
-		echo $par[2];
+	Router::get('/',function(){
+		
 	});
 
 ?>
